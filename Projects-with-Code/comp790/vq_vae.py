f3f842d38f8d138e4a15bcda17b7f84845759a5c
Qin Liu <qinliu19@cs.unc.edu>
2022-04-14 23:02:02 -0400

    Add vqvae

---
+++ Projects-with-Code/comp790/vq_vae.py
@@ -0,0 +1,403 @@
+from abc import abstractmethod
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import numpy as np
+import os
+from pathlib import Path
+import random
+import torch
+from torch import nn
+from torch.nn import functional as F
+import torchvision.transforms as transforms
+import torchvision.datasets.vision as vision
+import torchvision.io as io
+import torch.optim as optim
+from torchvision.utils import make_grid
+from typing import Any, TypeVar, Callable, Dict, List, Optional, Tuple
+
+
+class VectorQuantizer(nn.Module):
+    def __init__(self, num_embeddings, embedding_dim, commitment_cost):
+        super(VectorQuantizer, self).__init__()
+        
+        self._embedding_dim = embedding_dim
+        self._num_embeddings = num_embeddings
+        
+        self._embedding = nn.Embedding(self._num_embeddings, self._embedding_dim)
+        self._embedding.weight.data.uniform_(-1/self._num_embeddings, 1/self._num_embeddings)
+        self._commitment_cost = commitment_cost
+
+    def forward(self, inputs):
+        # convert inputs from BCHW -> BHWC
+        inputs = inputs.permute(0, 2, 3, 1).contiguous()
+        input_shape = inputs.shape
+        
+        # Flatten input
+        flat_input = inputs.view(-1, self._embedding_dim)
+        
+        # Calculate distances
+        distances = (torch.sum(flat_input**2, dim=1, keepdim=True) 
+                    + torch.sum(self._embedding.weight**2, dim=1)
+                    - 2 * torch.matmul(flat_input, self._embedding.weight.t()))
+            
+        # Encoding
+        encoding_indices = torch.argmin(distances, dim=1).unsqueeze(1)
+        encodings = torch.zeros(encoding_indices.shape[0], self._num_embeddings, device=inputs.device)
+        encodings.scatter_(1, encoding_indices, 1)
+        
+        # Quantize and unflatten
+        quantized = torch.matmul(encodings, self._embedding.weight).view(input_shape)
+        
+        # Loss
+        e_latent_loss = F.mse_loss(quantized.detach(), inputs)
+        q_latent_loss = F.mse_loss(quantized, inputs.detach())
+        loss = q_latent_loss + self._commitment_cost * e_latent_loss
+        
+        quantized = inputs + (quantized - inputs).detach()
+        avg_probs = torch.mean(encodings, dim=0)
+        perplexity = torch.exp(-torch.sum(avg_probs * torch.log(avg_probs + 1e-10)))
+        
+        # convert quantized from BHWC -> BCHW
+        return loss, quantized.permute(0, 3, 1, 2).contiguous(), perplexity, encodings
+
+
+class VectorQuantizerEMA(nn.Module):
+    def __init__(self, num_embeddings, embedding_dim, commitment_cost, decay, epsilon=1e-5):
+        super(VectorQuantizerEMA, self).__init__()
+        
+        self._embedding_dim = embedding_dim
+        self._num_embeddings = num_embeddings
+        
+        self._embedding = nn.Embedding(self._num_embeddings, self._embedding_dim)
+        self._embedding.weight.data.normal_()
+        self._commitment_cost = commitment_cost
+        
+        self.register_buffer('_ema_cluster_size', torch.zeros(num_embeddings))
+        self._ema_w = nn.Parameter(torch.Tensor(num_embeddings, self._embedding_dim))
+        self._ema_w.data.normal_()
+        
+        self._decay = decay
+        self._epsilon = epsilon
+
+    def forward(self, inputs):
+        # convert inputs from BCHW -> BHWC
+        inputs = inputs.permute(0, 2, 3, 1).contiguous()
+        input_shape = inputs.shape
+        
+        # Flatten input
+        flat_input = inputs.view(-1, self._embedding_dim)
+        
+        # Calculate distances
+        distances = (torch.sum(flat_input**2, dim=1, keepdim=True) 
+                    + torch.sum(self._embedding.weight**2, dim=1)
+                    - 2 * torch.matmul(flat_input, self._embedding.weight.t()))
+            
+        # Encoding
+        encoding_indices = torch.argmin(distances, dim=1).unsqueeze(1)
+        encodings = torch.zeros(encoding_indices.shape[0], self._num_embeddings, device=inputs.device)
+        encodings.scatter_(1, encoding_indices, 1)
+        
+        # Quantize and unflatten
+        quantized = torch.matmul(encodings, self._embedding.weight).view(input_shape)
+        
+        # Use EMA to update the embedding vectors
+        if self.training:
+            self._ema_cluster_size = self._ema_cluster_size * self._decay + \
+                                     (1 - self._decay) * torch.sum(encodings, 0)
+            
+            # Laplace smoothing of the cluster size
+            n = torch.sum(self._ema_cluster_size.data)
+            self._ema_cluster_size = (
+                (self._ema_cluster_size + self._epsilon)
+                / (n + self._num_embeddings * self._epsilon) * n)
+            
+            dw = torch.matmul(encodings.t(), flat_input)
+            self._ema_w = nn.Parameter(self._ema_w * self._decay + (1 - self._decay) * dw)
+            
+            self._embedding.weight = nn.Parameter(self._ema_w / self._ema_cluster_size.unsqueeze(1))
+        
+        # Loss
+        e_latent_loss = F.mse_loss(quantized.detach(), inputs)
+        loss = self._commitment_cost * e_latent_loss
+        
+        # Straight Through Estimator
+        quantized = inputs + (quantized - inputs).detach()
+        avg_probs = torch.mean(encodings, dim=0)
+        perplexity = torch.exp(-torch.sum(avg_probs * torch.log(avg_probs + 1e-10)))
+        
+        # convert quantized from BHWC -> BCHW
+        return loss, quantized.permute(0, 3, 1, 2).contiguous(), perplexity, encodings
+
+
+class Residual(nn.Module):
+    def __init__(self, in_channels, num_hiddens, num_residual_hiddens):
+        super(Residual, self).__init__()
+        self._block = nn.Sequential(
+            nn.ReLU(True),
+            nn.Conv2d(in_channels=in_channels,
+                      out_channels=num_residual_hiddens,
+                      kernel_size=3, stride=1, padding=1, bias=False),
+            nn.ReLU(True),
+            nn.Conv2d(in_channels=num_residual_hiddens,
+                      out_channels=num_hiddens,
+                      kernel_size=1, stride=1, bias=False)
+        )
+    
+    def forward(self, x):
+        return x + self._block(x)
+
+
+class ResidualStack(nn.Module):
+    def __init__(self, in_channels, num_hiddens, num_residual_layers, num_residual_hiddens):
+        super(ResidualStack, self).__init__()
+        self._num_residual_layers = num_residual_layers
+        self._layers = nn.ModuleList([Residual(in_channels, num_hiddens, num_residual_hiddens)
+                             for _ in range(self._num_residual_layers)])
+
+    def forward(self, x):
+        for i in range(self._num_residual_layers):
+            x = self._layers[i](x)
+        return F.relu(x)
+
+
+class Encoder(nn.Module):
+    def __init__(self, in_channels, num_hiddens, num_residual_layers, num_residual_hiddens):
+        super(Encoder, self).__init__()
+
+        self._conv_1 = nn.Conv2d(in_channels=in_channels,
+                                 out_channels=num_hiddens//2,
+                                 kernel_size=4,
+                                 stride=2, padding=1)
+        self._conv_2 = nn.Conv2d(in_channels=num_hiddens//2,
+                                 out_channels=num_hiddens,
+                                 kernel_size=4,
+                                 stride=2, padding=1)
+        self._conv_3 = nn.Conv2d(in_channels=num_hiddens,
+                                 out_channels=num_hiddens,
+                                 kernel_size=3,
+                                 stride=1, padding=1)
+        self._residual_stack = ResidualStack(in_channels=num_hiddens,
+                                             num_hiddens=num_hiddens,
+                                             num_residual_layers=num_residual_layers,
+                                             num_residual_hiddens=num_residual_hiddens)
+
+    def forward(self, inputs):
+        x = self._conv_1(inputs)
+        x = F.relu(x)
+        
+        x = self._conv_2(x)
+        x = F.relu(x)
+        
+        x = self._conv_3(x)
+        return self._residual_stack(x)
+
+
+class Decoder(nn.Module):
+    def __init__(self, in_channels, num_hiddens, num_residual_layers, num_residual_hiddens):
+        super(Decoder, self).__init__()
+        
+        self._conv_1 = nn.Conv2d(in_channels=in_channels,
+                                 out_channels=num_hiddens,
+                                 kernel_size=3, 
+                                 stride=1, padding=1)
+        
+        self._residual_stack = ResidualStack(in_channels=num_hiddens,
+                                             num_hiddens=num_hiddens,
+                                             num_residual_layers=num_residual_layers,
+                                             num_residual_hiddens=num_residual_hiddens)
+        
+        self._conv_trans_1 = nn.ConvTranspose2d(in_channels=num_hiddens, 
+                                                out_channels=num_hiddens//2,
+                                                kernel_size=4, 
+                                                stride=2, padding=1)
+        
+        self._conv_trans_2 = nn.ConvTranspose2d(in_channels=num_hiddens//2, 
+                                                out_channels=3,
+                                                kernel_size=4, 
+                                                stride=2, padding=1)
+
+    def forward(self, inputs):
+        x = self._conv_1(inputs)
+        
+        x = self._residual_stack(x)
+        
+        x = self._conv_trans_1(x)
+        x = F.relu(x)
+        
+        return self._conv_trans_2(x)
+
+
+class DAVIS(vision.VisionDataset):
+    def __init__(
+        self,
+        root: str,
+        num_frames: int = 10,
+        train: bool = True,
+        transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None,
+    ) -> None:
+        super().__init__(root, transform=transform, target_transform=target_transform)
+        self.num_frames = num_frames
+        self.train = train  # training set or test set
+        self.root = root
+
+        self.video_names = []
+        if train:
+            with open(os.path.join(root, 'ImageSets/2017/train.txt'), 'r') as f:
+                for line in f:
+                    self.video_names.append(line.strip('\n'))
+
+        else:
+            with open(os.path.join(root, 'ImageSets/2017/val.txt'), 'r') as f:
+                for line in f:
+                    self.video_names.append(line.strip('\n'))
+
+    def __getitem__(self, index: int) -> Any:
+        video_name = self.video_names[index]
+        video_frames = list((Path(self.root)/'JPEGImages/480p/{}'.format(video_name)).glob('*.jpg'))
+        video_frames.sort()
+
+        start_frame = random.randrange(len(video_frames) - self.num_frames)
+        video_frames = video_frames[start_frame: start_frame + self.num_frames]    
+
+        video_frames_data = self._load_data(video_frames)
+        video_frames_target = 0 # we don't need target
+
+        # random crop
+        random_crop = transforms.RandomCrop((480, 840), pad_if_needed=True)
+        resize = transforms.Resize((256, 512))
+        video_frames_data = resize(random_crop(video_frames_data))
+        video_frames_data = video_frames_data / 255 # maximum value for all images in DAVIS
+
+        return video_frames_data, video_frames_target
+
+    def __len__(self):
+        return len(self.video_names)
+
+    def _load_data(self, video_frames):
+        video_frames_data = []
+        for video_frame in video_frames:
+            frame_data = io.read_image(str(video_frame), mode=io.ImageReadMode.RGB)
+            video_frames_data.append(frame_data)
+        return torch.stack(video_frames_data, dim=0).float()
+
+
+class Model(nn.Module):
+    def __init__(self, num_hiddens, num_residual_layers, num_residual_hiddens, 
+                 num_embeddings, embedding_dim, commitment_cost, decay=0):
+        super(Model, self).__init__()
+        
+        self._encoder = Encoder(3, num_hiddens,
+                                num_residual_layers, 
+                                num_residual_hiddens)
+        self._pre_vq_conv = nn.Conv2d(in_channels=num_hiddens, 
+                                      out_channels=embedding_dim,
+                                      kernel_size=1, 
+                                      stride=1)
+        if decay > 0.0:
+            self._vq_vae = VectorQuantizerEMA(num_embeddings, embedding_dim, 
+                                              commitment_cost, decay)
+        else:
+            self._vq_vae = VectorQuantizer(num_embeddings, embedding_dim,
+                                           commitment_cost)
+        self._decoder = Decoder(embedding_dim,
+                                num_hiddens, 
+                                num_residual_layers, 
+                                num_residual_hiddens)
+
+    def forward(self, x):
+        z = self._encoder(x)
+        z = self._pre_vq_conv(z)
+        loss, quantized, perplexity, _ = self._vq_vae(z)
+        x_recon = self._decoder(quantized)
+
+        return loss, x_recon, perplexity
+
+
+device = torch.device("cuda:3")
+num_training_updates = 500000
+num_hiddens = 128
+num_residual_hiddens = 32
+num_residual_layers = 2
+embedding_dim = 64
+num_embeddings = 512
+commitment_cost = 0.25
+decay = 0.99
+learning_rate = 1e-3
+
+model = Model(num_hiddens, num_residual_layers, num_residual_hiddens,
+              num_embeddings, embedding_dim, 
+              commitment_cost, decay).to(device)
+
+optimizer = optim.Adam(model.parameters(), lr=learning_rate, amsgrad=False)
+
+
+## Dataset Parameters
+mb_size = 1 # each batch use only one video 
+num_frames = 12 # each video uses 10 consecutive frames for training
+davis_root = '/playpen-raid2/qinliu/data/DAVIS'
+
+
+trainset = DAVIS(root=davis_root, num_frames=num_frames, train=True)
+valset = DAVIS(root=davis_root, num_frames=num_frames, train=False)
+
+train_loader = torch.utils.data.DataLoader(trainset, batch_size=mb_size, 
+                                           shuffle=True, num_workers=1)
+
+val_loader = torch.utils.data.DataLoader(valset, batch_size=mb_size, 
+                                          shuffle=False, num_workers=1)
+
+
+train_res_recon_error = []
+train_res_perplexity = []
+
+model.train()
+fig_idx = 0
+for i in range(num_training_updates):
+    data, _ = next(iter(train_loader))
+    data = torch.squeeze(data, dim=0)
+    data = data.to(device)
+    optimizer.zero_grad()
+
+    vq_loss, data_recon, perplexity = model(data)
+    recon_error = F.mse_loss(data_recon, data)
+    loss = recon_error + vq_loss
+    loss.backward()
+
+    optimizer.step()
+    
+    train_res_recon_error.append(recon_error.item())
+    train_res_perplexity.append(perplexity.item())
+
+    if (i+1) % 20 == 0:
+        print('%d iterations' % (i+1))
+        print('recon_error: %.3f' % np.mean(train_res_recon_error[-100:]))
+        print('perplexity: %.3f' % np.mean(train_res_perplexity[-100:]))
+
+    if (i+1) % 1000 == 0:
+        model.eval()
+
+        valid_originals, _ = next(iter(val_loader))
+        valid_originals = torch.squeeze(valid_originals, dim=0)
+        valid_originals = valid_originals.to(device)
+
+        vq_output_eval = model._pre_vq_conv(model._encoder(valid_originals))
+        _, valid_quantize, _, _ = model._vq_vae(vq_output_eval)
+        valid_reconstructions = model._decoder(valid_quantize)
+
+        grid_img = make_grid(valid_reconstructions.cpu().data, nrow=4)
+        grid_img = grid_img.numpy()
+
+        fig = plt.figure(figsize=(16, 12))
+        plt.imshow(np.transpose(grid_img, (1,2,0)), interpolation='nearest')
+
+        if not os.path.exists('out/'):
+            os.makedirs('out/')
+
+        plt.savefig('out/{}.png'.format(str(fig_idx).zfill(3)), bbox_inches='tight')
+        fig_idx += 1
+        plt.close(fig)
+
+        model.train()
+
+torch.save(model.state_dict(), 'out/model.pt')
